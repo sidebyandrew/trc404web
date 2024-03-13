5f83619b6c6ae2955e24292e67b2ba53cc0cad21
Andrew <andrew.sideby@gmail.com>
2024-03-13 17:43:32 +0800

    To Make NFT Flow and Fly Universally.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,6 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: "TRC-404",
     description: "Probably not found.",
-    themeColor: [
-        {media: '(prefers-color-scheme: light)', color: 'white'},
-        {media: '(prefers-color-scheme: dark)', color: 'black'},
-    ],
     icons: {
         icon: '/favicon.ico',
         shortcut: '/favicon-16x16.png',
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,31 @@
-// app/providers.tsx
-'use client'
+'use client';
 
-import {NextUIProvider} from '@nextui-org/react'
-import {THEME, TonConnectUIProvider} from "@tonconnect/ui-react";
+import {NextUIProvider} from '@nextui-org/system';
+import {THEME, TonConnectUIProvider} from '@tonconnect/ui-react';
+import {ThemeProvider as NextThemesProvider} from 'next-themes';
+import {ThemeProviderProps} from 'next-themes/dist/types';
+import React from 'react';
 
-export function Providers({children}: { children: React.ReactNode }) {
+export interface ProvidersProps {
+    children: React.ReactNode;
+    themeProps?: ThemeProviderProps;
+}
+
+export function Providers({children, themeProps}: ProvidersProps) {
     return (
         <NextUIProvider>
+            {/* eslint-disable-next-line react/jsx-no-undef */}
             <TonConnectUIProvider
                 manifestUrl="https://trc404web.pages.dev/tonconnect-manifest.json"
                 uiPreferences={{
                     theme: THEME.DARK,
                 }}
                 actionsConfiguration={{
-                    twaReturnUrl: 'https://t.me/trc404bot/app',
+                    twaReturnUrl: 'https://t.me/ThePopcoinBot/app',
                 }}
             >
-                {children}
+                <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
             </TonConnectUIProvider>
         </NextUIProvider>
-    )
+    );
 }
