a66fdb481174e28367ea0767cb3fd1104531a7f3
Andrew <andrew.sideby@gmail.com>
2024-03-15 09:38:20 +0800

    master

--- src/components/tab/Tab1Content.tsx
+++ src/components/tab/Tab1Content.tsx
@@ -96,15 +96,15 @@ export default function Tab1Content() {
                 <Image
                     removeWrapper
                     alt="Relaxing app background"
-                    className="z-0 w-full h-full object-cover"
+                    className="z-0 w-full h-full object-fill"
                     src="/logos/fancy-vivid.png"
                 />
                 <CardFooter
                     className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                     <div className="flex flex-grow gap-2 items-center">
                         <div className="flex flex-col">
-                            <p className=" text-white/80">ERC-404</p>
-                            <p className="text-tiny text-white/80">To Make NFT Flow and Fly Universally.</p>
+                            <p className="text-white/80">ERC-404</p>
+                            <p className="text-tiny text-white/80">To Make NFT Flow and Fly!</p>
                         </div>
                     </div>
                 </CardFooter>
