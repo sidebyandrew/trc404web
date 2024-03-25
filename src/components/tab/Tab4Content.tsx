59220f6df83255e03b389504f6d757081f38f2b1
Andrew <andrew.sideby@gmail.com>
2024-03-25 19:36:26 +0800

    category

--- src/components/tab/Tab4Content.tsx
+++ src/components/tab/Tab4Content.tsx
@@ -5,6 +5,7 @@ import {Badge} from "@/components/ui/badge";
 
 interface Project {
     title: string;
+    category: string;
     badge: string;
     badgeVariant: string;
     desc: string;
@@ -14,6 +15,7 @@ interface Project {
 let projectList: Project[] = [
     {
         title: "Pink Market",
+        category: "DeFi",
         badge: "Building",
         badgeVariant: "green",
         desc: "404 Jetton Market build with order book.",
@@ -21,48 +23,56 @@ let projectList: Project[] = [
     },
     {
         title: "War of Floor Price",
+        category: "NFT",
         badge: "Building",
         badgeVariant: "green",
         desc: "Solve 404 NFT fast liquidity problem",
         content: "If you need Toncoin urgently, you can quickly sell the NFT at the floor price(maybe with discount.) "
     }, {
         title: "404 Factory",
+        category: "404",
         badge: "Planning",
         badgeVariant: "blue",
         desc: "Deploy your 404 project with pleasure",
         content: "Deploy your own 404 project with our standardized contract templates"
     }, {
         title: "Diamond 404",
+        category: "Staking",
         badge: "Planning",
         badgeVariant: "blue",
         desc: "A Lucky Draw Project",
         content: "Hold and stake T404 to get the chance to win BIG PRIZE!"
     }, {
         title: "Game loot box 404",
+        category: "GameFi",
         badge: "Partnership",
         badgeVariant: "purple",
         desc: "Games with TRC-404 build-in ",
         content: "Use TRC-404 protocol to develop your game."
     }, {
         title: "AIGC 404 NFT",
+        category: "AIGC",
         badge: "Partnership",
         badgeVariant: "purple",
         desc: "404 NFT with AIGC generated images",
         content: "Dynamically generate NFT images via AIGC, and use 404 to mix Jetton & NFT, which is a lot of fun!"
     }, {
         title: "Inscription 404",
+        category: "Inscription",
         badge: "Partnership",
         badgeVariant: "purple",
         desc: "Inscription & TRC-404",
         content: "Solve the liquidity problem of Inscription through the 404 protocol"
     }, {
         title: "Ticket 404",
+        category: "RWA",
         badge: "Partnership",
         badgeVariant: "purple",
         desc: "Ticket & TRC-404",
         content: "Bind tickets to the 404 protocol for easy ticket purchase, refund, and randomness"
     }, {
         title: "404 Uni DEX",
+        category: "DeFi",
         badge: "Partnership",
         badgeVariant: "purple",
         desc: "A DEX for 404 jettons",
@@ -80,19 +90,21 @@ export default function Tab4Bridge() {
                     let variant = project.badge === 'Building' ? 'green' : project.badge === 'Planning' ? 'blue' : project.badge === 'Partnership' ? "purple" : "gray";
                     return <>
                         <Card className="w-[350px] mb-2">
-                            <CardHeader className=" pt-2">
+                            <CardHeader className="p-3 pt-2">
                                 <CardTitle>
                                     <div className={"flex"}>
-                                        <div>{project.title}</div>
-                                        <div className={" ml-auto"}><Badge
-                                            variant={variant as any}>{project.badge}</Badge></div>
+                                        <div>{project.title} <span
+                                            className="text-gray-500 text-sm font-extralight">#{project.category}</span>
+                                        </div>
+                                        <div className={" ml-auto"}>
+
+                                            <Badge
+                                                variant={variant as any}>{project.badge}</Badge></div>
                                     </div>
-
-
                                 </CardTitle>
                                 <CardDescription>{project.desc} </CardDescription>
                             </CardHeader>
-                            <CardContent className="pb-2">
+                            <CardContent className="p-3 pb-2">
                                 {project.content}
                             </CardContent>
                         </Card>
